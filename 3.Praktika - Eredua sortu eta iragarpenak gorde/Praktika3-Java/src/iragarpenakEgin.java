06d371e95291eb5c530846b34c834dc8b182ee88
liamsu23 <liamsuarezcostumero@gmail.com>
2025-02-10 15:11:23 +0100

    3.Praktika

---
+++ 3.Praktika - Eredua sortu eta iragarpenak gorde/Praktika3-Java/src/iragarpenakEgin.java
@@ -0,0 +1,51 @@
+import weka.classifiers.Classifier;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
+public class iragarpenakEgin {
+    public void main(String[] args) throws Exception{
+        if (args.length < 3){
+            System.out.println("Argumentuak ondo sartu.");
+            return;
+        }
+        String modelPath = args[0];        // Ruta del modelo Naive Bayes
+        String testFilePath = args[1];     // Ruta del archivo test_blind.arff
+        String outputPath = args[2];       // Ruta del archivo de predicciones
+
+        try {
+            // 1 Cargar el modelo Naive Bayes entrenado
+            Classifier model = (Classifier) weka.core.SerializationHelper.read(modelPath);
+            System.out.println("Modelo cargado correctamente desde: " + modelPath);
+
+            // 2 Cargar el conjunto de test
+            DataSource source = new DataSource(testFilePath);
+            Instances testInstances = source.getDataSet();
+            if (testInstances.classIndex() == -1) {
+                testInstances.setClassIndex(testInstances.numAttributes() - 1);
+            }
+
+            // 3 Abrir el archivo para escribir las predicciones
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputPath))) {
+                writer.write("Instancia,Predicción\n");
+
+                // 4 Realizar predicciones sobre cada instancia
+                for (int i = 0; i < testInstances.numInstances(); i++) {
+                    double predIndex = model.classifyInstance(testInstances.instance(i));
+                    String predictedClass = testInstances.classAttribute().value((int) predIndex);
+
+                    // Escribir la predicción en el archivo
+                    writer.write((i + 1) + "," + predictedClass + "\n");
+                }
+
+                System.out.println("Predicciones guardadas en: " + outputPath);
+            }
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            System.out.println("Errorea.");
+        }
+    }
+}
